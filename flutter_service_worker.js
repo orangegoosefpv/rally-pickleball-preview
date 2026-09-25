7a62f768e8202bb02f762ded819748115743538c
Joshua C <joshua@users.noreply.github.com>
2026-09-24 20:27:15 -0400

    Deploy web preview: auth landing, real chat, match quality
    Source: orangegoosefpv/rally-pickleball@5985779

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,12 +19,12 @@ const RESOURCES = {"canvaskit/skwasm_heavy.js.symbols": "3c01ec03b5de6d62c34e170
 "assets/packages/flutter_map/lib/assets/flutter_map_logo.png": "208d63cc917af9713fc9572bd5c09362",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/AssetManifest.bin": "85a2cd159520152b0f89ac247cc3b242",
-"assets/fonts/MaterialIcons-Regular.otf": "3128bd685077a3b1233ec87f93d57509",
+"assets/fonts/MaterialIcons-Regular.otf": "2d52b27bddf99a1e640582f09681d133",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "073683681f9ebcc6721e4e1ba2dd12a8",
 "assets/AssetManifest.json": "daff98568f8999c2c32820c7aa750031",
 "assets/NOTICES": "b97ecde957c7846d71f6ee1b6958d2b3",
-"main.dart.js": "e7052e333920f828a5e3dc929188121d",
+"main.dart.js": "b65db565199b92eaef26aaee58a63b86",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -33,9 +33,9 @@ const RESOURCES = {"canvaskit/skwasm_heavy.js.symbols": "3c01ec03b5de6d62c34e170
 "version.json": "43f61f6d281cffe4bfbebae14c89d7a8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "6a9860eee28904f345ab1329aa251001",
-"index.html": "d403abe3cab738c2913aaef7d33e5d11",
-"/": "d403abe3cab738c2913aaef7d33e5d11",
-"flutter_bootstrap.js": "73a185b0c8435476a9afa6d4e0b12c70"};
+"index.html": "006e5a6458d47b3be3da13841d3cd1c9",
+"/": "006e5a6458d47b3be3da13841d3cd1c9",
+"flutter_bootstrap.js": "bf6b47229a67788e8ae3b4acba0d8a06"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
